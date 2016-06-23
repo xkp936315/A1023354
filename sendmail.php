442349932ff7c0270454d0e1f77e96116cb75dc7
xkp936315 <xkp936315@gmail.com>
2016-06-23 18:05:26 +0800

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -8,8 +8,8 @@
 	$mail->isSMTP();                                      // Set mailer to use SMTP
 	$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = 'xkp936315@gmail.com';                 // SMTP username
-	$mail->Password = 'wi0220lly';                           // SMTP password
+	$mail->Username = '';                 // SMTP username
+	$mail->Password = '';                           // SMTP password
 	//$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 587;                                    // TCP port to connect to
 
